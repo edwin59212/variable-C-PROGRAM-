026a6e30ecafe7848e7811de82444cc0f0fb3019
edwin59212 <85494777+edwin59212@users.noreply.github.com>
2021-07-09 13:15:44 +0300

    Add files via upload

---
+++ edu maina the hero.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+	float length,width,perimeter;
+	printf("Enter length of the rectangle:");
+	scanf("%f",&length);
+	printf("Enter width of the rectangle:");
+	scanf("%f",&width);
+	
+	perimeter=2*(length+width);
+	printf("perimeter of the rectangle=%f units",perimeter);
+	
+
+	return 0;
+	
+	
+}
